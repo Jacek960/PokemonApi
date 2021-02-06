b4475955994900d0ab345eb4a5445653e9cc573e
Jacek K <szachy960@gmail.com>
2021-02-06 14:17:29 +0100

    setting

--- pokemon_api/settings.py
+++ pokemon_api/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'g(xge+2kqcmsaeaev%jdl5-unape3jqzwnukw^n9$6efhj$ny)'
+
+#
+with open('/home/jacek/Desktop/coderslab/nowe_projekty/copia/pokemon_api/secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
